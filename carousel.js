51509800c3912ca59e3b5636958dfb43194546c4
MohammadMaasir <maasir554@gmail.com>
2024-12-10 04:17:57 +0530

    completed event gallery

---
+++ carousel.js
@@ -0,0 +1,60 @@
+const carouselLeft = document.getElementById("carousel-left")
+const carouselRight = document.getElementById("carousel-right")
+const carouselReel = document.getElementById("carousel-reel")
+
+const dots = document.getElementById("carousel-dots").children
+
+let activeIndex = 0;
+
+carouselRight.onclick = (evt) => {
+    dots[activeIndex].classList.remove("bg-primary")
+    dots[activeIndex].classList.add("bg-white")
+    
+    if(activeIndex<3){
+        activeIndex++
+        carouselReel.style.transition = ""
+    }
+    else{
+        activeIndex = 0;
+        carouselReel.style.transition = "none"
+    }
+    
+    dots[activeIndex].classList.remove("bg-white")
+    dots[activeIndex].classList.add("bg-primary")
+    carouselReel.style.transform = `translateX(-${100*activeIndex}%)`
+}
+
+carouselLeft.onclick = (evt) => {
+    dots[activeIndex].classList.remove("bg-primary")
+    dots[activeIndex].classList.add("bg-white")
+    
+    if(activeIndex>0){
+        carouselReel.style.transition = ""
+        activeIndex--
+    }
+    else{
+        activeIndex = 3;
+        carouselReel.style.transition = "none"
+    }
+    
+    dots[activeIndex].classList.remove("bg-white")
+    dots[activeIndex].classList.add("bg-primary")
+
+    carouselReel.style.transform = `translateX(-${100*activeIndex}%)`
+}
+
+
+for (let dot of dots){
+    dot.onclick = evt => {
+        dots[activeIndex].classList.remove("bg-primary")
+        dots[activeIndex].classList.add("bg-white")
+
+        activeIndex = dot.dataset.index;
+        
+        dots[activeIndex].classList.remove("bg-white")
+        dots[activeIndex].classList.add("bg-primary")
+        
+        carouselReel.style.transform = `translateX(-${100*activeIndex}%)`
+
+    }
+}
