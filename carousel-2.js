ac0007d8f6f188f4d80823b36f4802ee0c1a603f
MohammadMaasir <maasir554@gmail.com>
2024-12-26 04:30:34 +0530

    added testimonials section

--- carousel-2.js
+++ carousel-2.js
@@ -26,3 +26,31 @@ const swiper = new Swiper('#carousel-display', {
     },
 });
 
+
+const swiper2 = new Swiper('#testis-swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  autoplay: {
+      delay: 2500, // ms delay between transitions
+      disableOnInteraction: false, // Continue autoplay after user interaction
+    },
+
+  // If we need pagination
+  pagination: {
+    el: '#testis-bullets',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '#testis-right',
+    prevEl: '#testis-left',
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
